014ac01a708928d39a695dc1b4cbe169e4303bdd
bensig <bensig@gmail.com>
2018-09-04 21:40:03 -0700

    Updates to script notes and docker command.

--- rchain_docker.sh
+++ rchain_docker.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-# Yes, in the future, I want to change this to use a config.toml, but for now it's convenient in a single file... 
+# Updated 2018-9-4
+# This script will launch an RChain node on docker for the testnet.
+# Created by Ben Sigman - @benobi on RChain Discord
 
-# RCHAIN VARIABLES - You should change all of these.
+# Yes, in the future, I want to change this to use a config.toml, but for now it's convenient in a single file...
+
+# RCHAIN VARIABLES - You will need to change all of these.
 RCHAINVERSION="<rnode version to run>"
 RCHAINPRIVKEY="<insert priv key>"
 RCHAINBOOTSTRAP="<insert bootstrap node>"
 BONDSURL="<https url to bonds file>"
 WALLETURL="<https url to wallets file>"
+REQUIREDSIGS="10"
 
 # DOCKER VARIABLES - You may need to change these depending on if you followed my readme and created the same dockernetwork name I specified there.
 # This will open both of these ports below in iptables, but you may also need to open ports on your firewall if you have one (For example on AWS you will need to modify your security group to add these ports).
@@ -39,7 +44,7 @@ mv "$BONDSFILE" "bonds.txt"
 docker pull rchain/rnode:$RCHAINVERSION
 
 # Run docker command
-docker run -d -u root --name $DOCKERNAME --network $DOCKERNETWORK -it -p $RPCPORT:$RPCPORT -p $STATSPORT:$STATSPORT -v $LOCALDATADIR:$RCHAINDATADIR rchain/rnode:$RCHAINVERSION run --bootstrap $RCHAINBOOTSTRAP --bonds-file $RCHAINDATADIR/genesis/bonds.txt --wallets-file $RCHAINDATADIR/genesis/wallets.txt --deploy-timestamp 1 --required-sigs 10 --validator-private-key $RCHAINPRIVKEY -p $RPCPORT
+docker run -d -u root --name $DOCKERNAME --network $DOCKERNETWORK -it -p $RPCPORT:$RPCPORT -p $STATSPORT:$STATSPORT -v $LOCALDATADIR:$RCHAINDATADIR rchain/rnode:$RCHAINVERSION run --bootstrap $RCHAINBOOTSTRAP --bonds-file $RCHAINDATADIR/genesis/bonds.txt --wallets-file $RCHAINDATADIR/genesis/wallets.txt --deploy-timestamp 1 --required-sigs $REQUIREDSIGS --validator-private-key $RCHAINPRIVKEY -p $RPCPORT
 
 base64 -d <<<"H4sIAONTj1sAA71RQQ6AMAi78wqe61ESp4mJn+MlzmULG1R3c+HQFGgZ6L6oi3Qwe67QMZ8h6PYNtcd4amjTtD7hHRpf42q8mIjMNMSPZa00pEyn92G4hCgc+X60sBOZ/n3w9lVvv47eoumM2tS5gEp8b7R1yFnSSz7OmSrHZDbY0HhtUPnJgWSVIv7p0Q1vjkY1SQMAAA==" | gunzip
 
